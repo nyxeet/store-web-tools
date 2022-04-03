5dba733e7bb269a3adbcb49368a99a1a941d39a2
Dmytro Kyrychenko <dmytro.kyrychenko@plus4u.net>
2022-04-03 16:24:29 +0300

    fixed thunk type

--- src/redux/auth/actions.ts
+++ src/redux/auth/actions.ts
@@ -1,7 +1,16 @@
-import { Dispatch } from 'redux';
+import { Dispatch, Action, ActionCreator } from 'redux';
 import axios from 'axios';
 import { UserActionTypes, Request } from './types';
+import { ThunkAction } from 'redux-thunk';
+import { ApplicationState } from '../store';
 axios.defaults.baseURL = 'http://localhost:3001';
+
+export type AppThunk = ThunkAction<
+  void,
+  ApplicationState,
+  Request,
+  Action<string>
+>;
 
 const token = {
   set(token: string) {
@@ -14,24 +23,25 @@ const token = {
   },
 };
 
-export const register = (data: Request) => async (dispatch: Dispatch) => {
-  dispatch({
-    type: UserActionTypes.REGISTER_REQUEST,
-  });
-  try {
-    const response = await axios.post('/user/signup', data);
-    token.set(response.data.data.token);
-    return dispatch({
-      type: UserActionTypes.REGISTER_SUCCESS,
-      payload: response.data,
+export const register: ActionCreator<AppThunk> =
+  data => async (dispatch: Dispatch) => {
+    dispatch({
+      type: UserActionTypes.REGISTER_REQUEST,
     });
-  } catch (e: any) {
-    return dispatch({
-      type: UserActionTypes.REGISTER_ERROR,
-      payload: e.response.data,
-    });
-  }
-};
+    try {
+      const response = await axios.post('/user/signup', data);
+      token.set(response.data.data.token);
+      return dispatch({
+        type: UserActionTypes.REGISTER_SUCCESS,
+        payload: response.data,
+      });
+    } catch (e: any) {
+      return dispatch({
+        type: UserActionTypes.REGISTER_ERROR,
+        payload: e.response.data,
+      });
+    }
+  };
 
 export const login = (data: Request) => async (dispatch: Dispatch) => {
   dispatch({ type: UserActionTypes.LOGIN_REQUEST });
